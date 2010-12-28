54c5287d0637157aa2c60f692268d168b19588f8
Kazuhiro Osawa <yappo@shibuya.pl>
2010-12-28 21:29:32 +0900

    initial commit

---
+++ lib/Groonga.pm
@@ -0,0 +1,32 @@
+package Groonga;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Groonga -
+
+=head1 SYNOPSIS
+
+  use Groonga;
+
+=head1 DESCRIPTION
+
+Groonga is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
